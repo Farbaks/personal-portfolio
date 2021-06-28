418e9a08332be02bd69e1c93f139feab4c7d02f9
farbaks <faroukbakre@yahoo.com>
2021-06-28 15:49:17 +0100

    Fix: optimize page

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const compression = require('compression');
+const path = require('path');
+const app = express();
+// app.use(compression());
+
+// Serve static files....
+app.use(express.static(__dirname + '/dist/personal-website'));
+
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/personal-website/index.html'));
+});
+
+// default PORT
+app.listen(process.env.PORT || 80);
